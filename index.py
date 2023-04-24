b03d7662a3ea8ce1647b1544a572480f7cb84323
baishali <roybaishali07@gmail.com>
2023-04-24 22:16:01 +0530

    initial changes

--- index.py
+++ index.py
@@ -37,48 +37,84 @@ def authenticate():
 creds = authenticate()
 service = build(API_SERVICE_NAME, API_VERSION, credentials=creds)
 
+# Define the color codes dictionary
+color_codes = {
+    'LW': '5',  # BANANA
+    'ES': '9',   # BASIL
+    'EN': '6',   # TANGERINE
+    'NS': '8',   # GRAPHITE
+    'NN': '3',  # GRAPE
+}
+
 # Read the input file in CSV format
-with open('input.txt', 'r') as f:
+with open('input.csv', 'r') as f:
     reader = csv.DictReader(f)
     for row in reader:
-        # Parse the date from the input row
-        date_str = row['date']
-        date_obj = datetime.datetime.strptime(date_str, '%Y-%m-%d').date()
+        # Parse the start and end dates from the input row
+        start_date_str = row['start_date']
+        start_date_obj = datetime.datetime.strptime(start_date_str, '%Y-%m-%d').date()
+        end_date_str = row['end_date']
+        end_date_obj = datetime.datetime.strptime(end_date_str, '%Y-%m-%d').date()
 
-        # Set the start time of the event to 10 am on the specified date
-        start_time = datetime.datetime.combine(date_obj, datetime.time(10, 0, 0))
+        # Parse the start and end times from the input row
+        start_time_str = row['start_time']
+        start_time_obj = datetime.datetime.strptime(start_time_str, '%I%p').time()
+        end_time_str = row['end_time']
+        end_time_obj = datetime.datetime.strptime(end_time_str, '%I%p').time()
 
-        # Split the emails field by the | character and create a list of attendees
-        emails = row['emails'].split('|')
-        attendees = [{'email': email} for email in emails if email and '@' in email]
+        # Parse the weekdays from the input row
+        weekdays_str = row['weekdays']
+        weekdays = [x.strip()[:3] for x in weekdays_str.split('|')]
 
-        # Set the event location
-        location = row['location']
+        # Parse the attendees and types from the input row
+        attendees = row['attendees'].split('|')
+        types = row['type'].split('|')
 
-        # Create a new event on Google Calendar
-        event = {
-            'summary': row['summary'],
-            'description': row['description'],
-            'start': {
-                'dateTime': start_time.isoformat(),
-                'timeZone': 'Asia/Kolkata',
-            },
-            'end': {
-                'dateTime': (start_time + datetime.timedelta(hours=1)).isoformat(),
-                'timeZone': 'Asia/Kolkata',
-            },
-            'reminders': {
-                'useDefault': False,
-                'overrides': [
-                    {'method': 'email', 'minutes': 60},
-                    {'method': 'popup', 'minutes': 30},
-                ],
-            },
-            'attendees': attendees,
-            'location': location,
-        }
-        event = service.events().insert(calendarId='primary', body=event).execute()
+        # Loop through each date in the range between start_date and end_date
+        current_date_obj = start_date_obj
+        while current_date_obj <= end_date_obj:
+            # Check if the current date's weekday matches the specified weekdays
+            if current_date_obj.strftime('%a') in weekdays:
+                # Create a new event on Google Calendar
+                start_datetime = datetime.datetime.combine(current_date_obj, start_time_obj)
+                end_datetime = datetime.datetime.combine(current_date_obj, end_time_obj)
+                event = {
+                    'summary': row['summary'],
+                    'description': row['description'],
+                    'start': {
+                        'dateTime': start_datetime.isoformat(),
+                        'timeZone': 'Asia/Kolkata',
+                    },
+                    'end': {
+                        'dateTime': end_datetime.isoformat(),
+                        'timeZone': 'Asia/Kolkata',
+                    },
+                    'reminders': {
+                        'useDefault': False,
+                        'overrides': [
+                            {'method': 'email', 'minutes': 60},
+                            {'method': 'popup', 'minutes': 30},
+                        ],
+                    },
+                    'attendees': [],
+                    'location': row['location'],
+                    'colorId': color_codes.get(types[0], None)
+                }
+                # Add each attendee to the event
+                for i, attendee in enumerate(attendees):
+                    if attendee and '@' in attendee:
+                        attendee_obj = {
+                            'email': attendee,
+                            'displayName': attendee.split('@')[0],
+                            'responseStatus': 'needsAction'
+                        }
+                        event['attendees'].append(attendee_obj)
+                event = service.events().insert(calendarId='primary', body=event).execute()
 
-        # Write the event details to the output file
-        with open('output.txt', 'a') as f:
-            f.write(f'Event created: {event["htmlLink"]}\n')
+                with open('output.txt', 'a') as f:
+                    f.write(f'Event created for {current_date_obj}: {event["htmlLink"]}\n')
+
+            #Move to the next date
+            current_date_obj += datetime.timedelta(days=1)
+
+
