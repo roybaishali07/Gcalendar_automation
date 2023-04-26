47c2572c22884c5fbe4d6e737e4e4618a3469436
baishali <roybaishali07@gmail.com>
2023-04-27 00:07:29 +0530

    initial changes

--- index.py
+++ index.py
@@ -10,7 +10,7 @@ from googleapiclient.discovery import build
 # Define file paths
 CREDENTIALS_FILE = 'client_secret.json'
 TOKEN_FILE = 'token.pickle'
-INPUT_FILE = 'input.csv'
+INPUT_FILE = 'data/input.csv'
 OUTPUT_FILE = 'output.txt'
 
 # Define API scopes and version
@@ -50,6 +50,7 @@ color_codes = {
     'NN': '3',  # GRAPE
 }
 
+
 # Read the input file in CSV format
 with open('data/input.csv', 'r') as f:
     reader = csv.DictReader(f)
@@ -62,9 +63,9 @@ with open('data/input.csv', 'r') as f:
 
         # Parse the start and end times from the input row
         start_time_str = row['start_time']
-        start_time_obj = datetime.datetime.strptime(start_time_str, '%I%p').time()
+        start_time_obj = datetime.datetime.strptime(start_time_str, '%I.%M%p').time()
         end_time_str = row['end_time']
-        end_time_obj = datetime.datetime.strptime(end_time_str, '%I%p').time()
+        end_time_obj = datetime.datetime.strptime(end_time_str, '%I.%M%p').time()
 
         # Parse the weekdays from the input row
         weekdays_str = row['weekdays']
@@ -84,7 +85,6 @@ with open('data/input.csv', 'r') as f:
                 end_datetime = datetime.datetime.combine(current_date_obj, end_time_obj)
                 event = {
                     'summary': row['summary'],
-                    # 'description': row['description'],
                     'description': f"{row['description']} - View location on Google Maps: {row['gmap_link']}",
                     'start': {
                         'dateTime': start_datetime.isoformat(),
@@ -116,10 +116,83 @@ with open('data/input.csv', 'r') as f:
                         event['attendees'].append(attendee_obj)
                 event = service.events().insert(calendarId='primary', body=event).execute()
 
-                with open('data/output.txt', 'a') as f:
+                with open('output.txt', 'a') as f:
                     f.write(f'Event created for {current_date_obj}: {event["htmlLink"]}\n')
 
-            #Move to the next date
+            # Move to the next date
             current_date_obj += datetime.timedelta(days=1)
+
+
+# # Read the input file in CSV format
+# with open('data/input.csv', 'r') as f:
+#     reader = csv.DictReader(f)
+#     for row in reader:
+#         # Parse the start and end dates from the input row
+#         start_date_str = row['start_date']
+#         start_date_obj = datetime.datetime.strptime(start_date_str, '%Y-%m-%d').date()
+#         end_date_str = row['end_date']
+#         end_date_obj = datetime.datetime.strptime(end_date_str, '%Y-%m-%d').date()
+
+#         # Parse the start and end times from the input row
+#         start_time_str = row['start_time']
+#         start_time_obj = datetime.datetime.strptime(start_time_str, '%I%p').time()
+#         end_time_str = row['end_time']
+#         end_time_obj = datetime.datetime.strptime(end_time_str, '%I%p').time()
+
+#         # Parse the weekdays from the input row
+#         weekdays_str = row['weekdays']
+#         weekdays = [x.strip()[:3] for x in weekdays_str.split('|')]
+
+#         # Parse the attendees and types from the input row
+#         attendees = row['attendees'].split('|')
+#         types = row['type'].split('|')
+
+#         # Loop through each date in the range between start_date and end_date
+#         current_date_obj = start_date_obj
+#         while current_date_obj <= end_date_obj:
+#             # Check if the current date's weekday matches the specified weekdays
+#             if current_date_obj.strftime('%a') in weekdays:
+#                 # Create a new event on Google Calendar
+#                 start_datetime = datetime.datetime.combine(current_date_obj, start_time_obj)
+#                 end_datetime = datetime.datetime.combine(current_date_obj, end_time_obj)
+#                 event = {
+#                     'summary': row['summary'],
+#                     # 'description': row['description'],
+#                     'description': f"{row['description']} - View location on Google Maps: {row['gmap_link']}",
+#                     'start': {
+#                         'dateTime': start_datetime.isoformat(),
+#                         'timeZone': 'Asia/Kolkata',
+#                     },
+#                     'end': {
+#                         'dateTime': end_datetime.isoformat(),
+#                         'timeZone': 'Asia/Kolkata',
+#                     },
+#                     'reminders': {
+#                         'useDefault': False,
+#                         'overrides': [
+#                             {'method': 'email', 'minutes': 60},
+#                             {'method': 'popup', 'minutes': 30},
+#                         ],
+#                     },
+#                     'attendees': [],
+#                     'location': row['location'],
+#                     'colorId': color_codes.get(types[0], None)
+#                 }
+#                 # Add each attendee to the event
+#                 for i, attendee in enumerate(attendees):
+#                     if attendee and '@' in attendee:
+#                         attendee_obj = {
+#                             'email': attendee,
+#                             'displayName': attendee.split('@')[0],
+#                             'responseStatus': 'needsAction'
+#                         }
+#                         event['attendees'].append(attendee_obj)
+#                 event = service.events().insert(calendarId='primary', body=event).execute()
+
+#                 with open('data/output.txt', 'a') as f:
+#                     f.write(f'Event created for {current_date_obj}: {event["htmlLink"]}\n')
+
+#             #Move to the next date
+#             current_date_obj += datetime.timedelta(days=1)
 
 
