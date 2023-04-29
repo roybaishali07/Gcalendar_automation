d5d2c3e144ad258e73f03b8b6b4b7a4b6790d662
Anirban Dutta <duttashaan102@gmail.com>
2023-04-29 21:16:51 +0530

    added support for number of washes

--- index.py
+++ index.py
@@ -54,12 +54,19 @@ color_codes = {
 # Read the input file in CSV format
 with open('data/input.csv', 'r') as f:
     reader = csv.DictReader(f)
+    num_washes = 0
     for row in reader:
         # Parse the start and end dates from the input row
         start_date_str = row['start_date']
         start_date_obj = datetime.datetime.strptime(start_date_str, '%Y-%m-%d').date()
-        end_date_str = row['end_date']
-        end_date_obj = datetime.datetime.strptime(end_date_str, '%Y-%m-%d').date()
+        end_date_str = row['end_date/number_of_washes']
+        if end_date_str.isdigit():
+            num_washes = int(end_date_str)
+            print(num_washes)
+        else:
+            end_date_obj = datetime.datetime.strptime(end_date_str, '%Y-%m-%d').date()
+            print(end_date_obj)
+
 
         # Parse the start and end times from the input row
         start_time_str = row['start_time']
@@ -69,13 +76,29 @@ with open('data/input.csv', 'r') as f:
 
         # Parse the weekdays from the input row
         weekdays_str = row['weekdays']
-        weekdays = [x.strip()[:3] for x in weekdays_str.split('|')]
+        if '|' in weekdays_str:
+            weekdays = weekdays_str.split('|')
+        else:
+            weekdays =  weekdays_str.split(',')
 
         # Parse the attendees and types from the input row
-        attendees = row['attendees'].split('|')
-        types = row['type'].split('|')
+        if '|' in row['attendees']:
+            attendees = row['attendees'].split('|')
+        else:
+            attendees = row['attendees'].split(',')
+        if '|' in row['type']:
+            types = row['type'].split('|')
+        else:
+            types = row['type'].split('|')
 
+        if num_washes != 0:
+            num_weeks = num_washes//len(weekdays)
+            print(num_weeks)
+            num_days = num_weeks * 7
+            end_date_obj = start_date_obj + datetime.timedelta(days=num_days)
+            num_washes = 0
         # Loop through each date in the range between start_date and end_date
+        print("End date : ", end_date_obj)
         current_date_obj = start_date_obj
         while current_date_obj <= end_date_obj:
             # Check if the current date's weekday matches the specified weekdays
@@ -105,7 +128,7 @@ with open('data/input.csv', 'r') as f:
                     'location': row['location'],
                     'colorId': color_codes.get(types[0], None)
                 }
-                # Add each attendee to the event
+                #Add each attendee to the event
                 for i, attendee in enumerate(attendees):
                     if attendee and '@' in attendee:
                         attendee_obj = {
@@ -121,78 +144,4 @@ with open('data/input.csv', 'r') as f:
 
             # Move to the next date
             current_date_obj += datetime.timedelta(days=1)
-
-
-# # Read the input file in CSV format
-# with open('data/input.csv', 'r') as f:
-#     reader = csv.DictReader(f)
-#     for row in reader:
-#         # Parse the start and end dates from the input row
-#         start_date_str = row['start_date']
-#         start_date_obj = datetime.datetime.strptime(start_date_str, '%Y-%m-%d').date()
-#         end_date_str = row['end_date']
-#         end_date_obj = datetime.datetime.strptime(end_date_str, '%Y-%m-%d').date()
-
-#         # Parse the start and end times from the input row
-#         start_time_str = row['start_time']
-#         start_time_obj = datetime.datetime.strptime(start_time_str, '%I%p').time()
-#         end_time_str = row['end_time']
-#         end_time_obj = datetime.datetime.strptime(end_time_str, '%I%p').time()
-
-#         # Parse the weekdays from the input row
-#         weekdays_str = row['weekdays']
-#         weekdays = [x.strip()[:3] for x in weekdays_str.split('|')]
-
-#         # Parse the attendees and types from the input row
-#         attendees = row['attendees'].split('|')
-#         types = row['type'].split('|')
-
-#         # Loop through each date in the range between start_date and end_date
-#         current_date_obj = start_date_obj
-#         while current_date_obj <= end_date_obj:
-#             # Check if the current date's weekday matches the specified weekdays
-#             if current_date_obj.strftime('%a') in weekdays:
-#                 # Create a new event on Google Calendar
-#                 start_datetime = datetime.datetime.combine(current_date_obj, start_time_obj)
-#                 end_datetime = datetime.datetime.combine(current_date_obj, end_time_obj)
-#                 event = {
-#                     'summary': row['summary'],
-#                     # 'description': row['description'],
-#                     'description': f"{row['description']} - View location on Google Maps: {row['gmap_link']}",
-#                     'start': {
-#                         'dateTime': start_datetime.isoformat(),
-#                         'timeZone': 'Asia/Kolkata',
-#                     },
-#                     'end': {
-#                         'dateTime': end_datetime.isoformat(),
-#                         'timeZone': 'Asia/Kolkata',
-#                     },
-#                     'reminders': {
-#                         'useDefault': False,
-#                         'overrides': [
-#                             {'method': 'email', 'minutes': 60},
-#                             {'method': 'popup', 'minutes': 30},
-#                         ],
-#                     },
-#                     'attendees': [],
-#                     'location': row['location'],
-#                     'colorId': color_codes.get(types[0], None)
-#                 }
-#                 # Add each attendee to the event
-#                 for i, attendee in enumerate(attendees):
-#                     if attendee and '@' in attendee:
-#                         attendee_obj = {
-#                             'email': attendee,
-#                             'displayName': attendee.split('@')[0],
-#                             'responseStatus': 'needsAction'
-#                         }
-#                         event['attendees'].append(attendee_obj)
-#                 event = service.events().insert(calendarId='primary', body=event).execute()
-
-#                 with open('data/output.txt', 'a') as f:
-#                     f.write(f'Event created for {current_date_obj}: {event["htmlLink"]}\n')
-
-#             #Move to the next date
-#             current_date_obj += datetime.timedelta(days=1)
-
 
