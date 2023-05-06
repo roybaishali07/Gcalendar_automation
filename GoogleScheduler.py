2cfae70d90811cca0d3ee0b2788f18c437448144
baishali <roybaishali07@gmail.com>
2023-05-06 22:34:02 +0530

    added rescheduling_events

--- GoogleScheduler.py
+++ GoogleScheduler.py
@@ -165,12 +165,14 @@ def get_events(start_date, end_date):
             maxResults=1000,
             singleEvents=True,
             orderBy='startTime',
-            fields='items(summary,description,location,start,end,attendees,colorId)'
+            fields='items(summary,description,location,start,end,attendees)'
         ).execute()
     except HttpError as error:
         print(f'An error occurred: {error}')
     events = events_result.get('items', [])
     return events
+
+
 
 def read_dates():
     start_date_str = input("Enter start date in YYYY-MM-DD format: ")
@@ -197,7 +199,106 @@ def write_events(events):
             writer.writerow([date_str, start_time_str, end_time_str, weekday_str, summary, description, attendees, location])
 
 
+import csv
+from datetime import datetime, timedelta
 
+def get_events_for_res(service, date):
+    start_date = date.strftime("%Y-%m-%d")
+    end_date = (date + timedelta(days=1)).strftime("%Y-%m-%d")
+    events_result = service.events().list(
+        calendarId='primary',
+        timeMin=start_date + 'T00:00:00Z',
+        timeMax=end_date + 'T00:00:00Z',
+        maxResults=1000,
+        singleEvents=True,
+        orderBy='startTime',
+        fields='items(id,summary,description,location,start,end,attendees)'
+    ).execute()
+    events = events_result.get('items', [])
+    return events
+
+
+def reschedule_event(service):
+    # Specify the CSV file path
+    csv_file = 'data/reschedule.csv'
+
+    # Read the CSV file and retrieve the rescheduling information
+    with open(csv_file, 'r') as file:
+        reader = csv.DictReader(file)
+        rows = list(reader)
+
+    for row in rows:
+        if 'Old Date' not in row or 'New Date' not in row or 'New Time' not in row:
+            print("Invalid CSV file format. Please ensure the headers 'Old Date', 'New Date', and 'New Time' are present.")
+            continue
+
+        old_date_str = row['Old Date']
+        new_date_str = row['New Date']
+        new_time_str = row['New Time']
+
+        # Parse the rescheduling dates
+        try:
+            old_date = datetime.strptime(old_date_str, '%Y-%m-%d').date()
+            new_date = datetime.strptime(new_date_str, '%Y-%m-%d').date()
+        except ValueError:
+            print("Invalid date format in the CSV file.")
+            continue
+
+        # Parse the rescheduling time
+        try:
+            new_time = datetime.strptime(new_time_str, '%I:%M %p').time()
+        except ValueError:
+            print("Invalid time format in the CSV file.")
+            continue
+
+        # Retrieve events for the old date
+        events = get_events_for_res(service, old_date)
+
+        # Handle the case when no events are found
+        if not events:
+            print(f"No events found for the date {old_date_str}. Skipping to the next entry.")
+            continue
+
+        # Display the summaries of events for the old date
+        print(f"Events for {old_date_str}:")
+        for i, event in enumerate(events):
+            start_time = datetime.fromisoformat(event['start']['dateTime']).strftime('%I:%M %p')
+            summary = event['summary']
+            print(f"{i + 1}. {start_time} - {summary}")
+
+        # Prompt the user to select an event to reschedule
+        while True:
+            try:
+                event_num = input("Enter the number of the event you want to reschedule (or 'q' to skip): ")
+                if event_num == 'q':
+                    break
+                event_num = int(event_num)
+                if event_num < 1 or event_num > len(events):
+                    raise ValueError
+                break
+            except ValueError:
+                print("Invalid input. Please enter a number between 1 and", len(events))
+
+        if event_num == 'q':
+            continue
+
+        # Select the event to reschedule
+        event = events[event_num - 1]
+
+        # Reschedule the event to the new date and time
+        event['start']['dateTime'] = f"{new_date.isoformat()}T{new_time.strftime('%H:%M:%S')}"
+        event['end']['dateTime'] = f"{new_date.isoformat()}T{new_time.strftime('%H:%M:%S')}"
+
+        # Reschedule the event on Google Calendar
+        updated_event = service.events().update(
+            calendarId='primary',
+            eventId=event['id'],
+            body=event
+        ).execute()
+
+        print(f"Event '{updated_event['summary']}' rescheduled successfully on {new_date_str} at {new_time_str}.")
+
+    print("All events have been rescheduled.")
 
 
 def main():
@@ -213,6 +314,7 @@ def main():
         choice = input("""
         \n1. Create a new events from csv file.
 2. Fetch all events from calendar.
+3. Reschedule Event
 0.Exit.
         
 Enter your choice : """)
@@ -227,9 +329,11 @@ Enter your choice : """)
             write_events(events)
             print(f'Events written to {OUTPUT_CSV_FILE}')
             continue
+
+        if choice == '3':
+            reschedule_event(service)
         
         elif choice == '0':
             break
 
 main()
-        
